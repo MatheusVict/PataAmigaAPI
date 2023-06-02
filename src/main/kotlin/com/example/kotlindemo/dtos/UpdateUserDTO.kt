bf6b917ddfd6f981c0636e0a081b2361be32a13b
MatheusVict <matheusvictorhenrique@gmail.com>
2023-06-01 23:36:28 -0300

    updateUserDTO adjucts

--- src/main/kotlin/com/example/kotlindemo/dtos/UpdateUserDTO.kt
+++ src/main/kotlin/com/example/kotlindemo/dtos/UpdateUserDTO.kt
@@ -9,10 +9,6 @@ data class UpdateUserDTO(
   val name: String = "",
 
   @field:NotEmpty(message = "this field can not be empty")
-  @field:Email(message = "this field can not be empty")
-  val email: String = "",
-
-  @field:NotEmpty(message = "this field can not be empty")
   val location: String = "",
 
   @field:NotEmpty(message = "this field can not be empty")
@@ -24,18 +20,14 @@ data class UpdateUserDTO(
   @field:NotEmpty(message = "this field can not be empty")
   val phone: String = "",
 
-  @field:NotEmpty(message = "this field can not be empty")
   val instagram: String? = "",
 
-  @field:NotEmpty(message = "this field can not be empty")
   val facebook: String? = "",
 
-  @field:NotEmpty(message = "this field can not be empty")
   val whatsapp: String? = "",
 ) {
   fun toEntity(user: User): User {
     user.name = this.name
-    user.email = this.email
     user.location = this.location
     user.profilePic = this.profilePic
     user.banner = this.banner
